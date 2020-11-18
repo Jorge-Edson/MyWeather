bb24be7d43c42f68cb99f1624331d780375f7ee8
Jorge-Edson <60950040+Jorge-Edson@users.noreply.github.com>
2020-11-19 03:52:57 +0900

    Consumindo API de Clima HG Weather e adaptações no CSS.

--- www/components/app.js
+++ www/components/app.js
@@ -2,6 +2,30 @@ $(document).on('click', '#atualizar', function() {
   var onSuccess = function(position) {
     var lat = position.coords.latitude;
     var lon = position.coords.longitude;
+
+    $.ajax({
+      url: "https://api.hgbrasil.com/weather?format=json-cors&key=174b4728&lat=" + lat + "&lon="+ lon + "&user_ip=remote",
+      type: "get",
+      dataType: "json",
+      success: function(data){ //Executar se der certo
+
+      cidade = "<p>"+ data.results.city +"</p>";
+      clima = "<p><strong>Clima: </strong>"+ data.results.description +"</p>";
+      temperatura = "<p>"+ data.results.temp +"ºC</p>";
+
+      navigator.notification.alert("Aguarde...", atualizarDados(), "Carregando informações");
+
+      function atualizarDados()
+      {
+        $('#cidade').html(cidade);
+        $('#clima').html(clima);
+        $('#temperatura').html(temperatura);
+      }
+    },
+    error: function(){ //Executar se der erro
+      alert("Deu erro!");
+    }
+    })
     };
 
     function onError(error) {
